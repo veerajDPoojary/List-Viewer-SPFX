a45617182a2cab6291b930fb05799681210091b0
veerajdpoojary <veerajdpoojary@gmail.com>
2018-05-20 11:18:59 +0530

    first commit

---
+++ src/webparts/listViewer/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IListViewerWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ListViewerWebPartStrings' {
+  const strings: IListViewerWebPartStrings;
+  export = strings;
+}
